b373b72c0be9dc2159e89e431754ca0797b4b337
bot5020 <79709195+bot5020@users.noreply.github.com>
2021-05-07 19:14:33 +0400

    Add files via upload
    7.05

--- нужно.py
+++ нужно.py
@@ -7,8 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyautogui
 
-
-
 def op_prog(event):
     os.system('cd C:/Users/Степанчик/Desktop/скрипты')
     fill = os.listdir()
@@ -18,7 +16,7 @@ def op_prog(event):
     textik = Text(font='Arial 10')
     textik.pack()
     textik.insert(INSERT, fill)
-
+#
 def pr_google(event):
     driver = webdriver.Chrome(executable_path='C:/Users/Степанчик/Desktop/скрипты/chromedriver.exe')
     element = driver.get('https://google.gik-team.com/')
@@ -29,52 +27,54 @@ def pr_google(event):
     driver.find_element_by_xpath("/html/body/div/main/div[3]/div[2]/a[3]").click()
 
 def prost(event):
-    requests.get("https://www.google.ru/")
+    driver = webdriver.Chrome(executable_path='C:/Users/Степанчик/Desktop/скрипты/chromedriver.exe')
     txt = entry.get()
-    print(txt)
-    print(requests.post("https://www.google.ru/search?q=" + txt))
+    element = driver.get("https://www.google.ru/search?q=" + txt)
 
 def parol(event):
-    global pr
+    hlp = '1 = N' \
+          ' 2 = N @ ' \
+          '3 = n ' \
+          '4 = n @'
     txt = entry.get()
-    if txt == 1:
-        time.sleep(3)
+    textik = Text(font='Arial 10')
+    textik.pack()
+    textik.insert(INSERT, hlp)
+    if txt == '1':
+        print(1)
+        time.sleep(2)
         pyautogui.typewrite('Nrvbkrf1207')
-    elif txt == 2:
-        time.sleep(3)
+    elif txt == '2':
+        time.sleep(2)
         pyautogui.typewrite('Nrvbkrf@1207')
-    elif txt == 3:
-        time.sleep(3)
+    elif txt == '3':
+        time.sleep(2)
         pyautogui.typewrite('nrvbkrf1207')
-    elif txt == 4:
-        time.sleep(3)
+    elif txt == '4':
+        time.sleep(2)
         pyautogui.typewrite('nrvbkrf@1207')
     else:
-        time.sleep(3)
+        time.sleep(2)
         pyautogui.typewrite('stepan.mokeev160808@gmail.com')
 
-
-
-
-
 root = Tk()
-root.geometry('250x150')
+root.geometry('240x290')
 root.title("Полезная прога")
 root["bg"] = "gray22"
 
-op_proga = Button(width=10, height=1, text='Открывашка')
+op_proga = Button(width=14, height=2, text='Открывашка')
 op_proga.bind("<Button-1>", op_prog)
-op_proga.pack() #это кнопка которая открывайет скрипты
+op_proga.pack()  #это кнопка которая открывайет скрипты
 
-pr_googlee = Button(width=10, height=1, text='норм гуглить')
+pr_googlee = Button(width=14, height=2, text='норм гуглить')
 pr_googlee.bind("<Button-1>", pr_google)
 pr_googlee.pack()  #это кнопка которая отправляет в гугл более правильный текст и открывает его
 
-pros = Button(width=10, height=1, text='прост гугл')
+pros = Button(width=14, height=2, text='прост гугл')
 pros.bind("<Button-1>", prost)
 pros.pack()  #это кнопка которая будет задавть в гугл запрос
 
-prl = Button(width=10, height=1, text='Парольник')
+prl = Button(width=14, height=2, text='Парольник')
 prl.bind("<Button-1>", parol)
 prl.pack()
 
